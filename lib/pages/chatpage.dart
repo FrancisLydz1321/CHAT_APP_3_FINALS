7d75d92c33c6f7a033e780a0d73b8fe2c0cfa2bb
Eboy-99 <elijah.lydz@gmail.com>
2024-07-26 13:07:03 +0800

    CHATPAGE & SEARCH AKSJGAS:LGHJASG:LASHG:ALSGJH aahhhhh:

--- lib/pages/chatpage.dart
+++ lib/pages/chatpage.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app_3/pages/home.dart';
 import 'package:chat_app_3/service/database.dart';
 import 'package:chat_app_3/service/shared_pref.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -20,7 +21,6 @@ class _ChatPageState extends State<ChatPage> {
   String? myUserName, myProfilePic, myName, myEmail, messageId, chatRoomId;
   Stream? messageStream;
 
-
   getthesharedpref() async {
     myUserName = await SharedPreferenceHelper().getUserName();
     myProfilePic = await SharedPreferenceHelper().getUserPic();
@@ -33,6 +33,7 @@ class _ChatPageState extends State<ChatPage> {
 
   ontheload() async {
     await getthesharedpref();
+    await getAndSetMessages();
     setState(() {});
   }
 
@@ -49,20 +50,70 @@ class _ChatPageState extends State<ChatPage> {
       return "$a\_$b";
     }
   }
+
   Widget chatMessageTile(String message, bool sendByMe) {
-    return Row(mainAxisAlignment: sendByMe? MainAxisAlignment.end:MainAxisAlignment.start, children: [Flexible(child: Container(
-      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-      decoration: BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(24),topRight: Radius.circular(24), bottomLeft: sendByMe? Radius.circular(24): Radius.circular(0))
-    ))],);
+    // return Row(mainAxisAlignment: sendByMe ? MainAxisAlignment.end:MainAxisAlignment.start, children: [Flexible(child: Container(
+    //   margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+    //   decoration: BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(24),topRight: Radius.circular(24), bottomLeft: sendByMe? Radius.circular(24): Radius.circular(0))
+    // ))],);
+    return Row(
+      mainAxisAlignment:
+          sendByMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Flexible(
+            child: Container(
+          padding: EdgeInsets.all(16),
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24),
+              bottomRight: sendByMe ? Radius.circular(0) : Radius.circular(24),
+              topRight: Radius.circular(24),
+              bottomLeft: sendByMe ? Radius.circular(24) : Radius.circular(0),
+            ),
+            color: sendByMe
+                ? Color.fromARGB(255, 236, 236, 240)
+                : Color.fromARGB(
+                    255,
+                    211,
+                    228,
+                    243,
+                  ),
+          ),
+          child: Text(
+            message,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ))
+      ],
+    );
+    // ],);
   }
+
   Widget chatMessage() {
-    return StreamBuilder(stream: messageStream, builder: (context, AsyncSnapshot snapshot) {
-      return snapshot.hasData? ListView.builder(padding: EdgeInsets.only(bottom: 90,top: 130), itemCount: snapshot.data.docs.length, reverse: true, itemBuilder: (context,index){
-        DocumentSnapshot ds = snapshot.data.docs[index];
-        return ;
-      });
-    });
+    return StreamBuilder(
+        stream: messageStream,
+        builder: (context, AsyncSnapshot snapshot) {
+          return snapshot.hasData
+              ? ListView.builder(
+                  padding: EdgeInsets.only(bottom: 90, top: 130),
+                  itemCount: snapshot.data.docs.length,
+                  reverse: true,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot ds = snapshot.data.docs[index];
+                    return chatMessageTile(
+                        ds["message"], myUserName == ds["sendBy"]);
+                  })
+              : Center(
+                  child: CircularProgressIndicator(),
+                );
+        });
   }
+
   addMessage(bool sendClicked) {
     if (messagecontroller.text != "") {
       String message = messagecontroller.text;
@@ -101,152 +152,172 @@ class _ChatPageState extends State<ChatPage> {
     }
   }
 
+  getAndSetMessages() async {
+    messageStream = await DatabaseMethods().getChatRoomMessages(chatRoomId);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF7f30fe),
-        body: Container(
-          margin: EdgeInsets.only(
-            top: 60,
-            // left: 20,
-            // right: 20,
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: Row(
-                  children: [
-                    Icon(
+      backgroundColor: Color(0xFF7f30fe),
+      body: Container(
+        margin: EdgeInsets.only(
+          top: 60,
+          // left: 20,
+          // right: 20,
+        ),
+        child: Stack(
+          // Column()
+          children: [
+            chatMessage(),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Home(),
+                          ));
+                    },
+                    child: Icon(
                       Icons.arrow_back_ios_new_outlined,
                       color: Color(0xffc199cd),
                     ),
-                    SizedBox(
-                      width: 80,
+                  ),
+                  // Icon(
+                  //   Icons.arrow_back_ios_new_outlined,
+                  //   color: Color(0xffc199cd),
+                  // ),
+                  SizedBox(
+                    width: 80,
+                  ),
+                  Text(
+                    "PANSIS LEDUSTRES",
+                    style: TextStyle(
+                      color: Color(0xffc199cd),
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500,
                     ),
-                    Text(
-                      "PANSIS LEDUSTREs",
-                      style: TextStyle(
-                        color: Color(0xffc199cd),
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                padding:
-                    EdgeInsets.only(left: 20.0, right: 20, top: 50, bottom: 40),
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 1.15,
+            ),
+            // SizedBox(
+            //   height: 20,
+            // ),
+            // Container(
+            //   padding:
+            //       EdgeInsets.only(left: 20.0, right: 20, top: 50, bottom: 40),
+            //   width: MediaQuery.of(context).size.width,
+            //   height: MediaQuery.of(context).size.height / 1.15,
+            //   decoration: BoxDecoration(
+            //     color: Colors.white,
+            //     borderRadius: BorderRadius.only(
+            //       topLeft: Radius.circular(30),
+            //       topRight: Radius.circular(30),
+            //     ),
+            //   ),
+            //   child: Column(
+            //     children: [
+            //       Container(
+            //         padding: EdgeInsets.all(10),
+            //         margin: EdgeInsets.only(
+            //             left: MediaQuery.of(context).size.width / 2),
+            //         alignment: Alignment.bottomRight,
+            //         decoration: BoxDecoration(
+            //             color: Color.fromARGB(255, 10, 240, 240),
+            //             borderRadius: BorderRadius.only(
+            //               topLeft: Radius.circular(20),
+            //               topRight: Radius.circular(20),
+            //               bottomLeft: Radius.circular(20),
+            //             )),
+            //         child: Text(
+            //           'Hello how was the day?',
+            //           style: TextStyle(
+            //             color: Colors.black,
+            //             fontSize: 15,
+            //             fontWeight: FontWeight.w500,
+            //           ),
+            //         ),
+            //       ),
+            //       SizedBox(
+            //         height: 20,
+            //       ),
+            //       Container(
+            //         padding: EdgeInsets.all(10),
+            //         margin: EdgeInsets.only(
+            //             right: MediaQuery.of(context).size.width / 2),
+            //         alignment: Alignment.topLeft,
+            //         decoration: BoxDecoration(
+            //             color: Color.fromARGB(255, 10, 240, 240),
+            //             borderRadius: BorderRadius.only(
+            //               bottomRight: Radius.circular(20),
+            //               topRight: Radius.circular(20),
+            //               topLeft: Radius.circular(20),
+            //             )),
+            //         child: Text(
+            //           'Whatzz gooodd???',
+            //           style: TextStyle(
+            //             color: Colors.black,
+            //             fontSize: 15,
+            //             fontWeight: FontWeight.w500,
+            //           ),
+            //         ),
+            //       ),
+            // Spacer(),
+            Material(
+              elevation: 5,
+              borderRadius: BorderRadius.circular(30),
+              child: Container(
+                padding: EdgeInsets.all(10),
                 decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
+                  borderRadius: BorderRadius.circular(30),
                 ),
-                child: Column(
+                child: Row(
                   children: [
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      margin: EdgeInsets.only(
-                          left: MediaQuery.of(context).size.width / 2),
-                      alignment: Alignment.bottomRight,
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 10, 240, 240),
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            topRight: Radius.circular(20),
-                            bottomLeft: Radius.circular(20),
-                          )),
-                      child: Text(
-                        'Hello how was the day?',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
+                    Expanded(
+                      child: TextField(
+                        controller: messagecontroller,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Type a message",
+                          hintStyle: TextStyle(
+                            color: Colors.black45,
+                          ),
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      margin: EdgeInsets.only(
-                          right: MediaQuery.of(context).size.width / 2),
-                      alignment: Alignment.topLeft,
-                      decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 10, 240, 240),
-                          borderRadius: BorderRadius.only(
-                            bottomRight: Radius.circular(20),
-                            topRight: Radius.circular(20),
-                            topLeft: Radius.circular(20),
-                          )),
-                      child: Text(
-                        'Whatzz gooodd???',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ),
-                    Spacer(),
-                    Material(
-                      elevation: 5,
-                      borderRadius: BorderRadius.circular(30),
+                    GestureDetector(
+                      onTap: () {
+                        addMessage(true);
+                      },
                       child: Container(
                         padding: EdgeInsets.all(10),
                         decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: TextField(
-                                controller: messagecontroller,
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: "Type a message",
-                                  hintStyle: TextStyle(
-                                    color: Colors.black45,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                addMessage(true);
-                              },
-                              child: Container(
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                    color: Color(0xFFf3f3f3),
-                                    borderRadius: BorderRadius.circular(60)),
-                                child: Center(
-                                  child: Icon(
-                                    Icons.send,
-                                    color: Color(0xFFd3d3d3),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
+                            color: Color(0xFFf3f3f3),
+                            borderRadius: BorderRadius.circular(60)),
+                        child: Center(
+                          child: Icon(
+                            Icons.send,
+                            color: Color(0xFFd3d3d3),
+                          ),
                         ),
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
-            ],
-          ),
-        ));
+            )
+          ],
+        ),
+      ),
+      // ],
+      // ),),
+    );
   }
 }
